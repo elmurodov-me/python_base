06699dc9833e15ee69836b91a077a15177b1f34c
Nazir <Nazir@gmail.com>
2021-02-10 21:00:09 +0500

    first commit

---
+++ first.py
@@ -0,0 +1,81 @@
+print("nimadir text!")
+# int integer - butun sonlar 1,2,3,4....8
+#float haqiqiy sonlar 15.9, 1599.99, 
+# list = massivlar ['ajakjsjka',121,14.8,"akjsnkajsnk"]
+#string -  matn, satrlar tipi "nimadir so'z"
+#dictionary - lug'atlar key value
+
+# butun_son = 18127812717818
+# haqiqiy_son = 182198291.1928192819
+# massiv = ['nimadir',17,'yoshda',1590990.99,"so'm qarz"]
+# satr = "nimadir 17 yoshda 157890.99 so'm qarz"
+# lugat = {'qizil':1500000,'sariq':0,'yashil':0.0,'peshexod':1000000}
+
+
+# #begin1
+# import random 
+# a = random.randrange(1,1000)
+# print(a)
+# perimetr = 4*a
+# print(perimetr)
+
+# #begin2
+# import random 
+# a = random.randrange(1,1000)
+# print(a)
+# print("Kvadrat yuzasi:",a**2)#  son darajasini hosil qilish
+# print("Kvadrat yuzasi:",a*a)#  son darajasini hosil qilish
+
+# #begin3
+# import random 
+# a = random.randrange(1,100)
+# b = random.randrange(a,100)
+# print(a,b,end=' ')# yonma yon chiqarish
+# print("\n4burchak yuzasi:",a*b)#  son darajasini hosil qilish
+# print("4burchak perimetri:",2*(a+b))#  son darajasini hosil qilish
+
+
+# #begin4
+# import random
+# pi = 3.14# const - o'zgarmas qiymat
+# d = random.randrange(1,100)
+# print("aylana uzunligi:",d*pi)
+
+# #begin5
+# import random
+# a = random.randrange(1,100)
+# print("Kub yon tomoni:",a,"Kub hajmi:",a**3)
+# print("Kub yon tomoni:",a,"Kub yuzasi:",6*(a**2))
+
+
+# #begin 6
+# import random
+# a = random.randrange(1,10)
+# b = random.randrange(1,10)
+# c = random.randrange(1,10)
+# print(a,b,c,end=' ')
+# print("\nParallelepiped hajmi:",a*b*c)
+# print("\tParallelepiped yuzasi:",2*(a*b+b*c+a*c))
+
+# #begin7
+# import random
+# import math
+# r = random.randrange(1,10)
+# print(r)
+# print(2*math.pi*r)
+# print(math.pi*(r**2))
+
+# #begin9
+# import random
+# import math
+# a = random.randrange(1,10)
+# b = random.randrange(1,10)
+# print(a,b,end=' ')
+# print('\n',math.sqrt(a*b))
+
+#begin10
+import random
+a = random.randrange(1,10)
+b = random.randrange(1,10)
+print(a, ":", b)
+print(a+b,a*b,a**2,b**2,end=' ')
